57deb71044986f7a3d80ba0e49a2131b90f52f84
Danny Bentley <danny.bentley@som.com>
2017-05-04 19:29:37 -0700

    Added comments for read excel file

--- ExcelRead.py
+++ ExcelRead.py
@@ -5,15 +5,21 @@ from Microsoft.Office.Interop import Excel
 from System.Runtime.InteropServices import Marshal
 
 path = IN[0]
-ex = Excel.ApplicationClass()
-ex.Visible = False
-ex.DisplayAlerts = False
 
+# Instantiate the Excel Application
+ex = Excel.ApplicationClass()
+# Make it Visiable for us all to see
+ex.Visible = False
+# Disable Alerts - Errors Ignore them, they're probably not important
+ex.DisplayAlerts = False
+# Workbook 
 workbook = ex.Workbooks.Open(path)
+# WorkSheet
 ws = workbook.Worksheets[1]
+# Cell range
 x1range = ws.Range["A1", "A4"]
 x2range = ws.Range["B1", "B4"]
-
+ 
 r1 = x1range.Value2
 r2 = x2range.Value2
 
--- ExcelWrite.py
+++ ExcelWrite.py
@@ -2,28 +2,23 @@ import clr
 
 clr.AddReference('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
 from Microsoft.Office.Interop import Excel
-from System.Runtime.InteropServices import Marshal
 from System import Array
 
 path = IN[0]
-# Instantiate the Excel Application
+
 ex = Excel.ApplicationClass()
-# Make it Visiable for us all to see
 ex.Visible = True
-# Disable Alerts - Errors Ignore them, they're probably not important
 ex.DisplayAlerts = False
-# Workbook 
+
 workbook = ex.Workbooks.Open(path)
-# WorkSheet
 ws = workbook.Worksheets[1]
-# Cell range 
-xlrange = ws.Range["A1", "A6"]
-sheetName = IN[1]
 i = 0
-x = len(sheetName)
-a = Array.CreateInstance(object, x,1) # rows and column 
+x1range = ws.Range["A1", "A4"]
+sheetName = IN[1]
+a = Array.CreateInstance(object, len(sheetName),1) # row and column
+
 while i < len(sheetName):
     a[i,0] = sheetName[i]
     i += 1
 
-xlrange.Value2 = a
+x1range.Value2 = a
