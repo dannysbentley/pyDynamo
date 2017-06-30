519f4a95525f9862320cf913c26022c7191d48fd
Danny Bentley <danny.bentley@som.com>
2017-06-30 10:53:16 -0700

    Added Modify to Delete Elements.

---
+++ ModifyDeleteElements.py
@@ -0,0 +1,19 @@
+import clr
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+elements = UnwrapElement(IN[0])
+
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+for e in elements:
+    doc.Delete(e.Id)
+
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = "DONE"
