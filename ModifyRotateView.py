7897e207c5a5f140e537a51f888cae6f72ceac9e
Danny Bentley <danny.bentley@som.com>
2018-02-16 10:00:42 -0800

    fixed transactions on rotation of section.

--- ModifyRotateView.py
+++ ModifyRotateView.py
@@ -7,7 +7,7 @@ from Autodesk.Revit.DB import *
 
 clr.AddReference('RevitServices')
 from RevitServices.Persistence import DocumentManager
-from RevitServices.Transaction import TransactionManager
+from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
 
