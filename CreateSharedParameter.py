61a7077b5241d83eb971777af391dd95493a2c73
Danny Bentley <danny.bentley@som.com>
2018-03-16 13:40:43 -0700

    Added Create Shared Parameter sample

--- CreateSharedParameter.py
+++ CreateSharedParameter.py
@@ -18,20 +18,23 @@ doc = DocumentManager.Instance.CurrentDBDocument
 uidoc = DocumentManager.Instance.CurrentUIDocument
 app = doc.Application
 
+#Dynamo input
 tempFile = IN[0]
 category = IN[1]
-
+#get the category and build a category set. 
 builtInCategory = System.Enum.ToObject(BuiltInCategory, category.Id)
 cats = app.Create.NewCategorySet()
 cats.Insert(doc.Settings.Categories.get_Item(builtInCategory))
-
+#get the parameter file
 originalFile = app.SharedParametersFilename
 app.SharedParametersFilename = tempFile
 sharedParameterFile = app.OpenSharedParameterFile()
-
+#txt group name 
 GroupName = sharedParameterFile.Groups.get_Item("DYNAMO AND ADD-IN")
+#txt parameter name
 externalDefinition = GroupName.Definitions.get_Item("GROUP 1")
 
+#Create shared instance parameter. 
 TransactionManager.Instance.EnsureInTransaction(doc)
 newInstanceBinding = app.Create.NewInstanceBinding(cats)
 doc.ParameterBindings.Insert(externalDefinition, newInstanceBinding, BuiltInParameterGroup.PG_TEXT)
