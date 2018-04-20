5c3008fb9af8b2cb7279a27e6ea1a243abc296ef
Danny Bentley <danny.bentley@som.com>
2018-04-20 14:11:44 -0700

    Add Change Family Type sample

---
+++ ModifyChangeFamilyType.py
@@ -0,0 +1,20 @@
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import *
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+familyInstance = UnwrapElement(IN[0])
+familyType = UnwrapElement(IN[1])
+count = 0
+
+TransactionManager.Instance.EnsureInTransaction(doc)
+for item in familyInstance:
+    item.Symbol = familyType
+    count += 1
+TransactionManager.Instance.TransactionTaskDone()
