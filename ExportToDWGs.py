4be5294b2020a73375119e0615b3e30c72a5bcf7
Danny Bentley <danny.bentley@som.com>
2019-01-11 10:40:35 -0800

    Added DWG sample code

---
+++ ExportToDWGs.py
@@ -0,0 +1,47 @@
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+clr.AddReference('RevitServices')
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+import System
+from System.Collections.Generic import *
+
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+# def to export
+def ExportDwg(doc, path, fileName, view, options):
+	#Empty List to add ids
+	views = List[ElementId]()
+	#Convert list to Element Id list. 
+	for v in view:
+		views.Add(v.Id) #add view Ids.
+	#Set combined set to true. 
+	options.MergedViews = True
+	#The export from Document to AutoCAD files.
+	result = doc.Export(path, fileName[1], views, options)
+	
+	return result
+	
+#inputs 
+path = IN[0] # input file path
+fileName = IN[1] # input file name
+view = UnwrapElement(IN[2]) # input Sheets
+options = IN[3] # input company option.
+
+#Revit Document
+doc = DocumentManager.Instance.CurrentDBDocument
+
+#run def
+result = ExportDwg(doc, path, fileName, view, options)
+
+OUT = result
---
+++ ExportToDWGsExportOptions.py
@@ -0,0 +1,29 @@
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+clr.AddReference('RevitServices')
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+doc = DocumentManager.Instance.CurrentDBDocument
+options = None
+#check if the dwg export setting matches this name
+dwg_opt = "- SOM Struc Export"
+#collect all the settings in your project.
+settings = FilteredElementCollector(doc).WherePasses(ElementClassFilter(ExportDWGSettings))
+
+for element in settings:
+	if element.Name == dwg_opt:
+		options = element.GetDWGExportOptions()
+		break
+if options is None:
+	options = DWGExportOptions()
+	
+	
+OUT = options
