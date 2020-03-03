7137e73c09d99a8b67bc94c370f71f0db0a477cc
Danny <Bentley>
2020-03-03 09:29:11 +0700

    start script for dwp warnings

---
+++ WarningWallSlightlyOffAxis.py
@@ -0,0 +1,13 @@
+"""
+WarningWallSlightlyOffAxis
+"""
+__author__ = 'Danny Bentley - danny.b@dwp.com'
+__twitter__ = '@danbentley'
+__Website__ = 'http://dannybentley.tech/'
+__version__ = '1.0.0'
+
+"""
+This is a dynamo script to fix the warning for
+walls that are slightly off axis. 
+""" 
+
